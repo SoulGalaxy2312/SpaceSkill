11f01657ac16090910ada999d2d7a51a5af6280c
Ngô Thanh Trí <soulgalaxy@Ngos-MacBook-Air.local>
2025-05-04 15:18:39 +0700

    feat: add SecurityConfig, UserDetailsServiceImpl, and UserPrincipal for authentication

--- skillspace-backend/src/main/java/skillspace/skillspace_backend/User/controller/UserWriteController.java
+++ skillspace-backend/src/main/java/skillspace/skillspace_backend/User/controller/UserWriteController.java
@@ -3,6 +3,7 @@ package skillspace.skillspace_backend.User.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import skillspace.skillspace_backend.User.request.UserRegisterDTO;
 import skillspace.skillspace_backend.User.service.UserWriteService;
 import skillspace.skillspace_backend.shared.constants.ApiPath;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(ApiPath.AUTH)
+@Slf4j
 public class UserWriteController {
     private final UserWriteService userWriteService;
 
---
+++ skillspace-backend/src/main/java/skillspace/skillspace_backend/shared/security/UserPrincipal.java
@@ -0,0 +1,30 @@
+package skillspace.skillspace_backend.shared.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import skillspace.skillspace_backend.User.model.User;
+
+public class UserPrincipal implements UserDetails {
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+}
