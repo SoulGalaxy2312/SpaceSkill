ab9b0aa09fb452c5cbc7663f9e97cf9f36c524b4
Ngô Thanh Trí <ngotri2312@gmail.com>
2025-05-05 16:26:08 +0700

    feat: add profileName for getUserProfile api

--- skillspace-backend/src/main/java/skillspace/skillspace_backend/shared/model/BaseUser.java
+++ skillspace-backend/src/main/java/skillspace/skillspace_backend/shared/model/BaseUser.java
@@ -30,6 +30,9 @@ public abstract class BaseUser {
     @Column(nullable = false, unique = true)
     private String password;
 
+    @Column(nullable = false)
+    private String profileName;
+    
     private String location;
 
     @Enumerated(EnumType.STRING)
