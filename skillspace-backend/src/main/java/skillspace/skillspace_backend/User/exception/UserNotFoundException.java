7ad1e73c0604ed1252ffdf007d8e1f4bfdf29b56
Ngô Thanh Trí <ngotri2312@gmail.com>
2025-05-05 16:23:39 +0700

    feat: create and handle UserNotFoundException for checking if the userId exists or not

---
+++ skillspace-backend/src/main/java/skillspace/skillspace_backend/User/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package skillspace.skillspace_backend.User.exception;
+
+import java.util.UUID;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(UUID userId) {
+        super("User with ID " + userId + " was not found.");
+    }
+}
--- skillspace-backend/src/main/java/skillspace/skillspace_backend/shared/exception/GlobalExceptionHandler.java
+++ skillspace-backend/src/main/java/skillspace/skillspace_backend/shared/exception/GlobalExceptionHandler.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import skillspace.skillspace_backend.User.exception.UserNotFoundException;
+import skillspace.skillspace_backend.User.exception.UsernameExistsException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public String handleUsernameExistsException(UsernameExistsException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleUserNotFoundException(UserNotFoundException ex) {
         return ex.getMessage();
     }
 }
